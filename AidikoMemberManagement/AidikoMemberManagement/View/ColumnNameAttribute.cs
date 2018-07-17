624ec145b19f67bc3ab2e1e817adb043eb971f19
15110044 <15110044@student.hcmute.edu.vn>
2018-07-17 17:01:09 +0700

    Screen Design

---
+++ AidikoMemberManagement/AidikoMemberManagement/View/ColumnNameAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AidikoMemberManagement.View
+{
+    public class ColumnNameAttribute : Attribute
+    {
+        public ColumnNameAttribute(string Name) { this.Name = Name; }
+        public string Name { get; set; }
+
+    }
+}
