109d5710dc4340fd7b232af602ce9174093f1ee5
15110044 <15110044@student.hcmute.edu.vn>
2018-07-23 22:34:52 +0700

    Search Export

---
+++ Combine/Aikido/Aikido/VIEW/FeeScreen.xaml.cs
@@ -0,0 +1,210 @@
+﻿using Aikido.BLO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Aikido.DAO;
+namespace Aikido.VIEW
+{
+    /// <summary>
+    /// Interaction logic for FeeScreen.xaml
+    /// </summary>
+    public partial class FeeScreen : Window
+    {
+        ManageClass_BLO manageClass = new ManageClass_BLO();
+        private List<bool> btnSelect = new List<bool>();
+        public FeeScreen()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 5; i++)
+            {
+                btnSelect.Add(true);
+            }
+            btnSelect[2] = false;
+            manageClass = new ManageClass_BLO();
+            List<Class> showCombobox = manageClass.ComboxClass();
+            cmbClassName.ItemsSource = showCombobox;
+            cmbClassName.DisplayMemberPath = "Class_Name";
+            cmbClassName.SelectedValuePath = "ID_Class";
+        }
+
+        private void btnDKHV_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnDKHVb.Background = Brushes.DarkBlue;
+            btnDKHV.Background = Brushes.LightGray;
+        }
+
+        private void btnDKHV_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[0] == true)
+            {
+                btnDKHVb.Background = Brushes.White;
+                btnDKHV.Background = Brushes.White;
+            }
+        }
+
+        private void btnSearch_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnSearchb.Background = Brushes.DarkBlue;
+            btnSearch.Background = Brushes.LightGray;
+            btnSearchC.Visibility = Visibility.Visible;
+            btnSearchQ.Visibility = Visibility.Visible;
+        }
+
+        private void btnSearch_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[1] == true)
+            {
+                btnSearch.Background = Brushes.White;
+            }
+        }
+
+        private void btnQLHP_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnQLHPb.Background = Brushes.DarkBlue;
+            btnQLHP.Background = Brushes.LightGray;
+        }
+
+        private void btnQLHP_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[2] == true)
+            {
+                btnQLHPb.Background = Brushes.White;
+                btnQLHP.Background = Brushes.White;
+            }
+        }
+
+        private void btnQLL_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnQLLb.Background = Brushes.DarkBlue;
+            btnQLL.Background = Brushes.LightGray;
+        }
+
+        private void btnQLL_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[3] == true)
+            {
+                btnQLLb.Background = Brushes.White;
+                btnQLL.Background = Brushes.White;
+            }
+        }
+
+        private void btnTL_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnTLb.Background = Brushes.DarkBlue;
+            btnTL.Background = Brushes.LightGray;
+        }
+
+        private void btnTL_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[4] == true)
+            {
+                btnTLb.Background = Brushes.White;
+                btnTL.Background = Brushes.White;
+            }
+        }
+
+        private void btnDKHV_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            RegisterMemberScreen rgm = new RegisterMemberScreen();
+            rgm.Show();
+            this.Close();
+        }
+
+        private void btnSearch_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnQLHP_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //FeeScreen fs = new FeeScreen();
+            //fs.Show();
+            //this.Close();
+        }
+
+        private void btnQLL_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ClassScreen cs = new ClassScreen();
+            cs.Show();
+            this.Close();
+        }
+
+        private void btnTL_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SettingScreen sc = new SettingScreen();
+            sc.Show();
+            this.Close();
+        }
+
+        private void btnSearchQ_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnSearchb.Background = Brushes.DarkBlue;
+            if (btnSelect[1] == false)
+            {
+                btnSearchQ.Background = Brushes.LightGray;
+                btnSearchC.Background = Brushes.White;
+            }
+            else btnSearchQ.Background = Brushes.LightGray;
+        }
+
+        private void btnSearchQ_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[1] == true)
+            {
+                btnSearchQ.Background = Brushes.White;
+                btnSearchb.Background = Brushes.White;
+            }
+            btnSearchQ.Visibility = Visibility.Hidden;
+            btnSearchC.Visibility = Visibility.Hidden;
+        }
+
+        private void btnSearchQ_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            QuickSearch qs = new QuickSearch();
+            qs.Show();
+            this.Close();
+        }
+
+        private void btnSearchC_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[1] == false)
+            {
+                btnSearchC.Background = Brushes.LightGray;
+                btnSearchQ.Background = Brushes.White;
+            }
+            else btnSearchC.Background = Brushes.LightGray;
+            btnSearchb.Background = Brushes.DarkBlue;
+
+        }
+
+        private void btnSearchC_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (btnSelect[1] == true)
+            {
+                btnSearchC.Background = Brushes.White;
+                btnSearchb.Background = Brushes.White;
+            }
+            btnSearchQ.Visibility = Visibility.Hidden;
+            btnSearchC.Visibility = Visibility.Hidden;
+
+        }
+
+        private void btnSearchC_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SearchCondition sc = new SearchCondition();
+            sc.Show();
+            this.Close();
+        }
+    }
+}
